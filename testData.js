17f87259673ff902d7b8bce53426c428059acf9f
Igor Kapsky <46522997+Igor-Kapsky@users.noreply.github.com>
2023-02-06 13:09:33 +0100

    update test query

--- testData.js
+++ testData.js
@@ -1,6 +1,6 @@
 module.exports = class TestData {
     static query1 = {
-        select: ['project.name', 'test.name', 'MIN((test.end_time - test.start_time)) AS minDuration'],
+        select: ['project.name AS projectName', 'test.name', 'MIN((test.end_time - test.start_time)) AS minDuration'],
         from: 'test',
         join: { table : 'project', column1: 'test.project_id', column2: 'project.id' },
         group: ['project.name', 'test.name'],
@@ -13,7 +13,7 @@ module.exports = class TestData {
         group: ['project.name']
     };
     static query3 = {
-        select: ['project.name', 'test.name', 'test.start_time'],
+        select: ['project.name AS projectName', 'test.name', 'test.start_time'],
         from: 'test',
         join: { table : 'project', column1: 'test.project_id', column2: 'project.id' },
         where: 'test.start_time > "2015-11-07"',
